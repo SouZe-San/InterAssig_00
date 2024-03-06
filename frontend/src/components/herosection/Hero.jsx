1953ba8104c6ed90a7f10eca844a92db305f74e1
SouZe-San <2003soumyajit@gamil.com>
2024-03-07 04:04:22 +0530

    Design_done

--- frontend/src/components/herosection/Hero.jsx
+++ frontend/src/components/herosection/Hero.jsx
@@ -13,11 +13,19 @@ const Hero = () => {
           <span className="ml-16"></span>We Are
         </h1>
         <h1 className="text-8xl font-bold">
-          Here to Show Others <span className="special_word">Thoughts </span>
+          Here to Show Others <br />
+        </h1>
+        <h1 className="text-8xl font-bold relative">
+          <span className="special_word">Thoughts </span>
+          <div className="text-block">
+            <span className="ml-16"></span>
+            We are fueled by Your passion for creating distinctive experiences that set you apart
+            from the sea of ordinary brands in today&apos;s market.
+          </div>
         </h1>
       </div>
 
-      <div className="w-screen flex justify-center mt-16 scroll_btn_container">
+      <div className="w-screen flex justify-center mt-[10vh] scroll_btn_container">
         <ScrollBtn />
       </div>
     </div>
